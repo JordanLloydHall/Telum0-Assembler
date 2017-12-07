cd210b3c3c6cbe801ca666102f5fe2a87973c8c1
PlatinumNinja72 <Jordanhall123@googlemail.com>
2017-12-07 18:55:08 +0000

    Restarted, remade part of assembler

--- example.asm
+++ example.asm
@@ -1,11 +1,16 @@
-section .data
-; Data/Variables go here
-hello 5
-lmao 22
-
 
 section .text
 ; Code goes here
-ADD A B
-SUB B A
-LDA B [122]
+
+_start:
+ADD A, B
+SUB B, A
+LDA B, 122
+
+section .data
+; Data/Variables go here
+hello db 5
+lmao db 22
+
+section .bss
+
---
+++ helper.py
@@ -0,0 +1,25 @@
+
+def process(file):
+	file = file.split("\n")
+	pFile = []
+	tokFile = []
+
+	for line in range(len(file)):
+		file[line] = file[line].upper()
+		pFile.append("")
+
+		for char in range(len(file[line])):
+			
+
+			if file[line][char] == ";":
+				break
+			else:
+				pFile[line] += file[line][char]
+	
+	for line in pFile:
+		if line == "":
+			continue
+		else:
+			tokFile.append([x.replace(" ", "").replace(",", "") for x in line.split(" ")])
+
+	return tokFile
